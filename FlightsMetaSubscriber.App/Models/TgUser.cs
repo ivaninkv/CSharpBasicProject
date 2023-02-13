3dfd9ac75a5f8b981cea336ffb8d1e931f4725be
Konstantin Ivanin <ivanin.k@gmail.com>
2023-02-13 18:04:05 +0300

    Added constructor with all fields for TgUser

--- FlightsMetaSubscriber.App/Models/TgUser.cs
+++ FlightsMetaSubscriber.App/Models/TgUser.cs
@@ -15,6 +15,13 @@ public class TgUser
         Active = active;
     }
 
+    public TgUser(long userId, string? userName, bool active)
+    {
+        Id = userId;
+        UserName = userName;
+        Active = active;
+    }
+
     public long Id { get; }
     public string? UserName { get; set; }
     public bool Active { get; set; }
