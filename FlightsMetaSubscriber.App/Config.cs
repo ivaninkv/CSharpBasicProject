64901961345681228b906e929b2204665a28ed1e
Konstantin <ivanin.k@gmail.com>
2023-02-14 10:39:43 +0300

    Fix typo in env name

--- FlightsMetaSubscriber.App/Config.cs
+++ FlightsMetaSubscriber.App/Config.cs
@@ -2,7 +2,7 @@ namespace FlightsMetaSubscriber.App;
 
 public static class Config
 {
-    private static readonly string? dbHost = Environment.GetEnvironmentVariable("DB_NAME");
+    private static readonly string? dbHost = Environment.GetEnvironmentVariable("DB_HOST");
     private static readonly string? dbName = Environment.GetEnvironmentVariable("DB_NAME");
     private static readonly string? dbPass = Environment.GetEnvironmentVariable("DB_PASS");
 
