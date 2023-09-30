8084b90c6034a8499c1fe32a9c539a4f1d68c0c7
Konstantin Ivanin <ivanin.k@gmail.com>
2023-09-30 10:25:53 +0300

    Change pre-deploy

--- FlightsMetaSubscriber.App/pre-deploy.sh
+++ FlightsMetaSubscriber.App/pre-deploy.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-USERNAME="postgres"
-
-# Проверить наличие пользователя
-if id -u $USERNAME >/dev/null 2>&1; then
-  echo "User $USERNAME already exists."
-else
-  # Создать пользователя без возможности входа в систему
-  sudo useradd --shell /usr/sbin/nologin $USERNAME
-  echo "User $USERNAME created without login."
-fi
-
 # Массив с путями к директориям для проверки и создания
-DIRECTORIES=("/logs/FlightsMetaSubscriber.App" "/psql-data/FlightsMetaSubscriber.App/pgdata")
+DIRECTORIES=("/logs/FlightsMetaSubscriber.App")
 
 # Перебрать массив директорий
 for DIRECTORY in "${DIRECTORIES[@]}"; do
@@ -21,11 +10,6 @@ for DIRECTORY in "${DIRECTORIES[@]}"; do
     # Создать папку
     mkdir -p "$DIRECTORY"
     echo "Directory $DIRECTORY created."
-    if [ "$DIRECTORY" = "/psql-data/FlightsMetaSubscriber.App/pgdata" ]; then
-      chown "$USERNAME:$USERNAME" "$DIRECTORY"
-      chmod 777 "$DIRECTORY"
-      echo "Changed permissions and owner for $DIRECTORY"
-    fi
   else
     echo "Directory $DIRECTORY already exists."
   fi
